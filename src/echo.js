250f7804a14abc970777887d8e8dddc0bc7e3982
Marcin Wosinek <marcin@wosinek.eu>
2021-05-15 12:46:33 +0100

    add range import

---
+++ src/echo.js
@@ -0,0 +1,3 @@
+const element = document.getElementById("echo");
+
+element.innerHTML = "Hello world";
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import range from "../node_modules/underscore/modules/range.js";
+
 import "./echo.js";
 
 const element = document.getElementById("load-status");
 
-element.innerHTML = "✅ js is working";
+element.innerHTML = "✅ js is working " + range(1, 10).join(", ");
